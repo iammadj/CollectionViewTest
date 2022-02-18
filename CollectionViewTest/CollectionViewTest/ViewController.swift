bda261f93d15f56f07169af7060a98762956a5fc
Majit Uteniyazov <iammadj.u@gmail.com>
2022-02-18 13:53:33 +0500

    initial commit
    Created project

---
+++ CollectionViewTest/CollectionViewTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CollectionViewTest
+//
+//  Created by Majit Uteniyazov on 18/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
